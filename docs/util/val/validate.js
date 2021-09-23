b3b7a262941f8ee3940692021faab6267c6ea1de
Gon <29622355+gonyyi@users.noreply.github.com>
2021-09-23 17:57:42 -0500

    Update validate.js

--- docs/util/val/validate.js
+++ docs/util/val/validate.js
@@ -1,88 +1,66 @@
-// validate.js
-//    (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright.txt>
-// Usage: (using class name defined in body.onload, and it uses title attr)
-//    <body onload="Validate('valhtml',true);">
-//    ...
-//       <li class="valhtml" url="https://gonyyi.com"></li>
-var gyyStatusC1 = "";
-var baseURL = "";
-function gyyStatusCls() {
-  var style = document.createElement('style');
-  style.type = 'text/css';
-  style.innerHTML = 
-    `.${gyyStatusC1}_fail,.${gyyStatusC1}_ok,.${gyyStatusC1}_unknown 
-      {display:inline-block; border:1px solid gray; background-color:gray; 
-        padding:0 .5em; margin: 0 0 0em 1em; border-radius: 1em;
-        text-align: center; font-size: .8em;
-        background-color:white; color: gray;}
-      .${gyyStatusC1}_fail {border-color: #FF4A31;color:white;background-color: #FF4A31;}
-      .${gyyStatusC1}_ok {border-color: #397EFF;color:white;background-color:#397EFF;}
-      .msg {padding: .1em .5em; margin: 0 0 0 2em; border-radius:0; 
-        border: .2em solid #999; border-width: 0 0 0 .4em; 
-        background-color:transparent; font-size:.8em; text-transform:none;}
-      .res_error {border-color:#ff0000;} 
-      .res_io {border-color:#ff9000;}
-      .res_warning {border-color:#ffdd00;}`;
-  document.getElementsByTagName('head')[0].appendChild(style);
-}
-function gyyStatusUpBtn(DST, addClass, content) {
-  URL = DST.getAttribute("title");
-  // console.log(`baseURL=${baseURL}; URL=${URL}`);
+<!DOCTYPE html> 
+<html lang="en">
+  <!-- Copyright (c) Gon Y. Yi 2016-2021 <https://gonyyi.com/copyright> -->
+  <head>
+    <title>gon/y/yi</title>
+    <base id="baseTarget" target="_self" href="https://gonyyi.com"/>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0"/>
+    <meta property="og:locale" content="en_US"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://gonyyi.com"/>
+    <meta property="og:image" content="img/og-img.png"/>
+    <meta property="og:site_name" content="gonyyi"/>
+    <meta property="og:title" content="validator"/>
+    <link rel="icon" type="image/svg+xml" href="img/favicon.svg"/>
+    <link rel="alternate icon" type="image/png" href="img/favicon.png"/>
+    <link rel="alternate icon" href="favicon.ico"/>
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png" sizes="any"/>
+    <link rel="stylesheet" type="text/css" href="css/base.css"/>
+    
+    <script src="util/val/validate.min.js"></script>
+    <!-- <script src="validate.js"></script> -->
+    <style>
+      /* this prevents entire thing from becoming block */
+      ul li a {display: inline-block;}
+    </style>
+   </head>
+   <!-- When onLoad run GYYStatus(CLASS_NAME, USE_DEFAULT_CSS); -->
+   <body onload="Validate('valhtml',true,'https://gonyyi.com');">
 
-  // first anchor
-  {
-    let a = document.createElement('a');
-    a.href = baseURL+URL;
-    a.innerHTML = URL;
-    DST.appendChild(a);
-  }
-  
-  {
-    a = document.createElement('a');
-    a.href = `https://validator.w3.org/nu/?doc=${baseURL}${URL}`;
-    // a.innerHTML = URL;
-    span = document.createElement('span');
-    span.classList.add(gyyStatusC1+"_"+addClass);
-    span.innerHTML = content.length;
-    a.appendChild(span);
-    DST.appendChild(a);
-  }
-  // second anchor
-  
-  if( content.length > 0) {
-    for(i=0;i<content.length;i++) {
-      pre = document.createElement("pre");
-      errType = (content[i].subType ? content[i].subType : content[i].type);
-      pre.classList.add("msg", `res_${errType}`);
-      pre.innerHTML = i+". [" + errType.toUpperCase() +"] ";
-      pre.innerHTML += content[i].message;
-      DST.appendChild(pre);
-    }
-  }
-}
-function gyyStatusChk(DST) {
-  URL=DST.getAttribute("title")
-  fetch(`https://validator.w3.org/nu/?out=json&doc=${baseURL}${URL}`)
-  .then(res=>res.json())
-  .then(res=>{
-    if(res.messages.length > 0) {
-      gyyStatusUpBtn(DST, 'fail', res.messages);
-    } else {
-      gyyStatusUpBtn(DST, 'ok', res.messages);
-    }
-  })
-  .catch(err => {
-    if (err instanceof TypeError) {
-      gyyStatusUpBtn(DST, 'unknown', []); // 503 unreachable
-    } else {
-      console.error("WARN: fetch("+URL+"}) ==> "+err);
-    }
-  });
-}
-function Validate(CSSClassName, UseDefaultCSS, UseBaseURL) {
-  console.log("(c) Gon Yi 2021 <https://gonyyi.com/copyright>");
-  baseURL = UseBaseURL;
-  gyyStatusC1 = CSSClassName;
-  if(UseDefaultCSS==true) {gyyStatusCls();}
-  Array.from(document.getElementsByClassName(gyyStatusC1)).forEach(c => gyyStatusChk(c));
-}
+    <h1><a href="https://gonyyi.com">gon/y/yi</a></h1>
+
+    <hr/>
+    <h2>essential</h2>
+    <small>HTML validation for essential pages</small>
+    <ul>
+      <li class="valhtml" title="/index.html"></li>
+      <li class="valhtml" title="/404.html"></li>
+      <li class="valhtml" title="/copyright/index.html"></li>
+      <li class="valhtml" title="/contact/index.html"></li>
+      <li class="valhtml" title="/lost/index.html"></li>
+      <li class="valhtml" title="/img/index.html"></li>
+    </ul>
+
+    <hr/>
+    <h2>utility</h2>
+    <ul>
+      <li class="valhtml" title="/util/dsyim/index.html"></li>
+      <li class="valhtml" title="/util/tmpl/index.html"></li>
+      <li class="valhtml" title="/util/val/index.html"></li>
+    </ul>
+
+    <hr/>
+    <h2>temporary</h2>
+    <ul>
+      <li class="valhtml" title="/tmp/darkmode.html"></li>
+      <li class="valhtml" title="/tmp/sitemap.html"></li>
+      <li class="valhtml" title="/tmp/status.html"></li>
+      <li class="valhtml" title="/tmp/tooltip.html"></li>
+      <li class="valhtml" title="/tmp/url.html"></li>
+    </ul>
+
+    <hr/>
+    <footer>Copyright &copy; Gon Y. Yi 2016-2021</footer>
+  </body>
+</html>
