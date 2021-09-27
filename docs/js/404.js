442edfa89abee5f5ad0580d69a245f137596ff4e
Gon <29622355+gonyyi@users.noreply.github.com>
2021-09-27 14:17:01 -0500

    Create 404.js

---
+++ docs/js/404.js
@@ -0,0 +1,106 @@
+// Copyright (c) 2021 Gon Y. Yi <https://gonyyi.com/copyright>
+console.log("DSYiM Search Keyword - (c) 2021 gon/y/yi");
+
+//
+// GLOBAL VARIABLES
+//
+var maxRes, keyword, sugg, keys;
+
+//
+// MATCH ALGORITHM 
+//
+function bigrams(string){
+  var str = string.toLowerCase()
+  var chars = str.split('');
+  for(var i=0; i<chars.length; i++){ chars[i] = str.slice(i, i + 2); }
+  return chars;
+}
+function similarity(str1="", str2=""){
+  if(str1.length>0 && str2.length>0){
+    var bgstr1 = bigrams(str1);
+    var bgstr2 = bigrams(str2);
+    var union = bgstr1.length + bgstr2.length;
+    var matches = 0;
+    for(var i=0; i<bgstr1.length; i++){
+      for(var j=0; j<bgstr2.length; j++){
+        if(bgstr1[i]==bgstr2[j]) matches++;
+    }}
+    if(matches>0) return ((2.0 * matches) / union);
+  }
+  return 0.0
+}
+
+//
+// SEARCH
+//
+function search() {
+  tmpRes = [];
+  keys.forEach(function(k){
+      highestScore = 0;
+      for(i = 0; i < k.keywords.length; i++) {
+          score=similarity(k.keywords[i], keyword);
+          // console.log(k.name, keyword, score);
+          if(score>highestScore) {
+              highestScore = score;
+          }  
+      }
+      if(highestScore > 0.2) {
+          tmpRes.push( {data:k, score:highestScore} );
+      }
+  })
+  // console.log(tmpRes);
+  if(tmpRes.length>0) {
+    tmpResIdx = Object.keys(tmpRes)
+    .sort(function(item1,item2){
+      if(tmpRes[item1].score > tmpRes[item2].score) return 1;
+      if(tmpRes[item1].score < tmpRes[item2].score) return -1;
+      if(tmpRes[item1].data.keywords > tmpRes[item2].data.keywords) return -1;
+      if(tmpRes[item1].data.keywords < tmpRes[item2].data.keywords) return 1;
+      return 0;
+      // return tmpRes[item1].score - tmpRes[item2].score
+    }).reverse();
+    // limit how many can print
+    if(maxRes != -1 && tmpResIdx.length > maxRes) {
+      tmpResIdx = tmpResIdx.slice(0, maxRes);
+    }
+
+    // If found a match, 
+    if(tmpResIdx.length > 0) {
+      tmpResIdx.forEach(function(k){
+        resultAdd(tmpRes[k]);
+      });
+    }
+  } else {
+    resultEmpty();
+  }
+}
+
+//
+// RESULT
+//
+function resultAdd(elm) {
+  // tmp_score = " (match: "+elm.score.toFixed(2)+")";
+  tmp_li = document.createElement("li");
+  
+  tmp_a = document.createElement('a');
+  tmp_a.setAttribute('href', elm.data.url);
+  tmp_a.innerHTML = elm.data.url
+  tmp_li.appendChild(tmp_a);
+  tmp_i = document.createElement('i');
+  // tmp_i.innerHTML = elm.data.keywords + tmp_score;
+  tmp_i.innerHTML = ` (${elm.score.toFixed(2)})`;
+  tmp_li.appendChild(tmp_i);
+  sugg.appendChild(tmp_li);
+}     
+
+//
+// RUN
+//
+function suggestURLs(idSuggestion, maxResult) {
+  keyword = window.location.pathname.replaceAll("/", " ").trim();      
+  // keyword = window.location.toString().split("?")[1];
+  // console.log("Seach for " + keyword);
+  sugg = document.getElementById(idSuggestion);
+  maxRes = maxResult;
+  search();
+}
